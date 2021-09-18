647a8ebe2712d336449e2b385421d235adf36260
TheIronFortress <K_Brooksby@hotmail.com>
2021-09-18 15:03:14 -0600

    Big Update
    1. Fixed jittery bending.
    2. Added 2 forms of jumping using rigidbody physics.
    3. Added the ability to disable and enable movement controls.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,34 +11,106 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float maxRotationAngle;
     [SerializeField] float rotationSpeed;
     [SerializeField] float bounciness;
+    [SerializeField] float jumpDelay; // in milliseconds
     [SerializeField] Transform pivot;
     [SerializeField] Transform top;
     [SerializeField] Transform bottom;
 
     // Player Hiddens
+    bool canMove;
+    bool jumpIntent;
     Rigidbody2D rb;
+    int currentJumps;
+    float jumpTimer;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        canMove = true;
+        jumpIntent = false;
         rb = GetComponent<Rigidbody2D>();
+        currentJumps = numberOfJumps;
+        jumpTimer = jumpDelay;
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    // Update is called on every frame.
+    private void Update()
     {
+        // Increment timers
+        jumpTimer += Time.deltaTime;
 
+        // Determines if the player wants to use a jump
+        if (Input.GetKey("space"))
+        {
+            jumpIntent = true;
+        }
     }
 
     // FixedUpdate is called regularly. This is for executing movement controls.
     private void FixedUpdate()
     {
-        float targetAngle = top.rotation.eulerAngles.z + rotationSpeed * Input.GetAxis("Horizontal");
-        if (-maxRotationAngle < targetAngle && targetAngle + 360 > 360 - maxRotationAngle) 
+        if (canMove)
         {
-            top.RotateAround(pivot.position, Vector3.back, rotationSpeed * Input.GetAxis("Horizontal"));
-            bottom.RotateAround(pivot.position, Vector3.forward, rotationSpeed * Input.GetAxis("Horizontal"));
+            Move();
+            Jump();
         }
-        Debug.Log(targetAngle);
+    }
+
+    private void Move() 
+    {
+        float targetRotation = top.localRotation.eulerAngles.z + rotationSpeed * Input.GetAxis("Horizontal");
+
+        if (maxRotationAngle < targetRotation && targetRotation < 180f)
+            targetRotation = maxRotationAngle;
+        else if (180f < targetRotation && targetRotation < 360f - maxRotationAngle)
+            targetRotation = 360f - maxRotationAngle;
+
+        top.localRotation = Quaternion.Euler(0f, 0f, targetRotation);
+        bottom.localRotation = Quaternion.Euler(0f, 0f, 360f - targetRotation);
+    }
+
+    // Jump is currently broken, is not registering key presses
+    private void Jump() 
+    {
+        if (currentJumps > 0) 
+        {
+            if (jumpIntent) 
+            {
+                if (jumpTimer >= jumpDelay)
+                {
+                    float intentDirection = Input.GetAxis("Horizontal");
+                    //if (intentDirection == 0)
+                    //    rb.AddForce(Vector2.up * jumpPower);
+                    //else
+                    //    rb.angularVelocity += jumpPower * ((intentDirection > 0) ? -1 : 1);
+
+                    Vector2 direction = Vector2.up;
+                    if (intentDirection != 0)
+                        direction = (((intentDirection > 0) ? -1 : 1) * Vector2.left + direction).normalized;
+                    rb.AddForce(direction * jumpPower);
+
+                    // Decrements relevant trackers on a successful jump attempt.
+                    currentJumps -= 1;
+                    jumpTimer = 0;
+                }
+                jumpIntent = false;
+                Debug.Log("Remaining Jumps: " + currentJumps);
+            }
+        }
+    }
+
+    private void ResetRun()
+    {
+        currentJumps = numberOfJumps;
+    }
+    private void DisableMovement() 
+    {
+        canMove = false;
+    }
+
+    private void EnableMovement() 
+    {
+        canMove = true;
     }
 }
